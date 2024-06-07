f2a38356598993bf025c761b41a29ae32e992110
Sesh-Gallagher <t.ntwana@gmail.com>
2024-06-07 07:10:19 -0700

    Binary trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,14 +19,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_height_a - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height.
  *
  * Return: If tree is NULL, your function must return 0
  *
  */
-
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height_a(const binary_tree_t *tree)
 {
 		size_t l = 0;
 		size_t r = 0;
@@ -37,8 +36,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		}
 		else
 		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			if (tree)
+			{
+				l = tree->left ? 1 + binary_tree_height_a(tree->left) : 1;
+				r = tree->right ? 1 + binary_tree_height_a(tree->right) : 1;
 		}
 		return (l > r ? l : r);
+	}
 }
