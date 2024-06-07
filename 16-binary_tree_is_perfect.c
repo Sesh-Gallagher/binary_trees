6bb80d33a4fba4533e9ce5c968544d1d1851b5b4
Sesh-Gallagher <t.ntwana@gmail.com>
2024-06-07 08:40:10 -0700

    Binary trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,9 +10,9 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t power = 0
-	size_t power = 0
 	size_t height = 0;
+	size_t nodes = 0;
+	size_t power = 0;
 
 	if (!tree)
 		return (0);
@@ -20,22 +20,19 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree->right && !tree->left)
 		return (1);
 
-	nodes = binary_tree_size(tree);
 	height = binary_tree_height(tree);
+	nodes = binary_tree_size(tree);
 
 	power = (size_t)_pow_recursion(2, height + 1);
 	return (power - 1 == nodes);
-
 }
 
 /**
- *_pow_recursion - Function that returns value of x raised to the power of y
+ *_pow_recursion - returns the value of x raised to the power of y
  *@x: is the value to exponentiate
  *@y: is the power to raise x to
- *Return: x to the power of y, or -1 if y is negative
- *
+ *Return: x to the power of y, or -1 if y is Null
  */
-
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
@@ -44,27 +41,7 @@ int _pow_recursion(int x, int y)
 		return (1);
 	else
 		return (x * _pow_recursion(x, y - 1));
-}
 
-/**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height.
- *
- * Return: If tree is NULL, your function must return 0
- *
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-		size_t height_l = 0;
-		size_t height_r = 0;
-
-		if (!tree)
-			return (0);
-
-		height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return (height_l > height_r ? height_l : height_r);
 }
 
 /**
@@ -81,4 +58,25 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		return (0);
 
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0
+ *
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
