a6ccdf80f9918edde46ceb1ecc337768c43d5bbb
Sesh-Gallagher <t.ntwana@gmail.com>
2024-06-07 08:45:07 -0700

    Binary trees fix

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -27,10 +27,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (!node || !node->parent)
-                return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-        if (node == node->parent->left)
-                return (node->parent->right);
-        return (node->parent->left);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
